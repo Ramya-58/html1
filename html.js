f2a216a05937e0506eed8a3954086b8e07d4509b
Ramya-58 <chowdaryr082@gmail.com>
2024-11-22 21:56:51 +0530

    Add files via upload
    html

---
+++ html.js
@@ -0,0 +1,13 @@
+<!DocumentType html>
+<html lang='en'>
+    <head>
+        <meta charset="UTF-8">
+        </meta>
+        <title>
+            <h1>Document type</h1>
+        </title>
+    </head>
+    <body>
+        <p> Hello world</p>
+    </body>
+</html>
